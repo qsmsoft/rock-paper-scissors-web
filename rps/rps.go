d361b2da475bcb95cf6be1b7f2a14cbd8dcb3999
qsmsoft <qsmsoft@gmail.com>
2021-11-05 00:41:23 +0100

    webapp

---
+++ rps/rps.go
@@ -0,0 +1,60 @@
+package rps
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	ROCK         = 0 // beats scissors. (scissors + 1) % 3 = 0
+	PAPER        = 1 // beats rock. (rock + 1) % 3 = 1
+	SCISSORS     = 2 // beats paper. (paper + 1) % 3 = 2
+	PLAYERWINS   = 1
+	COMPUTERWINS = 2
+	DRAW         = 3
+)
+
+type Round struct {
+	Winner         int    `json:"winner"`
+	ComputerChoice string `json:"computer_choice"`
+	RoundResult    string `json:"round_result"`
+}
+
+func PlayRound(playerValue int) Round {
+	rand.Seed(time.Now().UnixNano())
+	computerValue := rand.Intn(3)
+	computerChoise := ""
+	roundResult := ""
+	winner := 0
+
+	switch computerValue {
+	case ROCK:
+		computerChoise = "Computer chose ROCK"
+		break
+	case PAPER:
+		computerChoise = "Computer chose PAPER"
+		break
+	case SCISSORS:
+		computerChoise = "Computer chose SCISSORS"
+		break
+	default:
+	}
+
+	if playerValue == computerValue {
+		roundResult = "It's a draw"
+		winner = DRAW
+	} else if playerValue == (computerValue+1)%3 {
+		roundResult = "Player wins!"
+		winner = PLAYERWINS
+	} else {
+		roundResult = "Computer wins!"
+		winner = COMPUTERWINS
+	}
+
+	var result Round
+	result.Winner = winner
+	result.ComputerChoice = computerChoise
+	result.RoundResult = roundResult
+
+	return result
+}
